7922de02a42be73a7adef39be1f3d982251a03b3
Cl1493 <152617155+Cl1493@users.noreply.github.com>
2024-02-29 17:21:44 +0000

    Boton

--- src/app.js
+++ src/app.js
@@ -1,20 +1,42 @@
-function randomPalo(){
-    let palos=["♥","♠","♦","♣"];
-    return palos[Math.floor(Math.random() * palos.length)];
-};
-function randomNumero(){
-    let numeros=['A','2','3','4','5','6','7','8','9','J','Q','K'];
-    return numeros[Math.floor(Math.random() * numeros.length)];
-};
-    
-console.log(randomPalo());
-console.log(randomNumero());
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+// import "./index.html";
 
-window.addEventListener('load', function()) {
-for (let i=0; i>12;i++){
-    let paloDeCarta = document.querySelector('.palo');
-    let numeroDeCarta = document.querySelector('.numero');
-    paloDeCarta.innerHTML = randomPalo;
-    numeroDeCarta.innerHTML = randomNumero;
+window.onload = function() {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNum();
 };
-}
+
+let button = document.getElementById("button");
+button.addEventListener("click", () => {
+  let card = document.querySelector(".card");
+  let removeClassName = card.classList["1"];
+  document.querySelector(".card").classList.remove(removeClassName);
+  card.classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNum();
+});
+let generateRandomNum = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
